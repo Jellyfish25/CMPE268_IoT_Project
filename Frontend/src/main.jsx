2d9a8dad5a1e27e80981eb397d7487b164803407
Dimitar Dimitrov <mitkodimitrov50@gmail.com>
2024-10-20 16:24:56 -0700

    Incorporated current temperature readings + css

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Root from "./routes/root";
 import ErrorPage from "./routes/error/error-page";
 import Dashboard from "./routes/Dashboard/Dashboard";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: "/Dashboard",
     element: <Dashboard/>,
     errorElement: <ErrorPage/>
   },
--- Frontend/src/routes/Dashboard/Dashboard.jsx
+++ Frontend/src/routes/Dashboard/Dashboard.jsx
@@ -1,8 +1,63 @@
-import * as React from "react";
+import React, {useState, useEffect} from "react";
+import "./Dashboard.css"
 
 const Dashboard = () => {
+    const [temperature, setTemperature] = useState(0.0);
+    const [tempInput, setTempInput] = useState(0.0);
+
+    // read data and set the temperature to be displayed (so far set to default 50.0)
+    useEffect(() => {
+        setTemperature(50.0);
+    }, [])
+
+    useEffect(() => {
+        const temperatureBarFill = document.querySelector(".temperature-bar-fill");
+        if (temperatureBarFill) {
+            temperatureBarFill.style.width = temperature + "%";
+        }
+    }, [temperature])
+
+    const processSubmit = () => {
+        let temp = parseFloat(tempInput)
+        if(!isNaN(temp)) {
+            temp = Math.max(0, Math.min(temp, 100));
+            setTemperature(temp);
+        }
+        else {
+            alert("Please enter a valid temperature value");
+        }
+    }
+
     return (
-        <h1>Test Page</h1>
+        <div>
+            <h1>Current Temperature</h1>
+            <div className="container"> 
+                <div className="temperature-bar">
+                    <div className="temperature-bar-fill"></div> 
+                </div>
+                <div className="scale">
+                        <div>0</div><div>10</div><div>20</div><div>30</div><div>40</div>
+                        <div>50</div><div>60</div><div>70</div><div>80</div><div>90</div>
+                        <div>100</div>
+                </div>
+
+                <div style={{marginTop: "2.5%"}}>
+                        Detected Temperature: {temperature}
+                </div>
+
+                <div className="input-container">
+                    <input
+                        name="temperature-input"
+                        type="number"
+                        value={tempInput}
+                        onChange={(e) => setTempInput(e.target.value)
+                        }
+                        style={{width: "20%", marginRight: "5%"}}
+                    />
+                    <button onClick={processSubmit}>Set Temperature</button>
+                </div>
+            </div>
+        </div>
     );
 }
 
